54d5006f57d9a2e2dac221a484e23a7641795d20
Владимир Сидоров <svs_13@mail.ru>
2018-07-02 12:35:36 +0300

    lesson3.Work on bugs 4

--- homework3/calculator.php
+++ homework3/calculator.php
@@ -10,19 +10,28 @@ include __DIR__ . '/functions.php';
     <body>
 
         <?php
-        $n1 = '';
-        $op = '';
-        $n2 = '';
-        $res = 0;
-        $operations = ['+', '-', '*', '/', '^'];
-
-        if ( isset($_GET['n1'], $_GET['op'], $_GET['n2']) && is_numeric($_GET['n1']) && is_numeric($_GET['n2']) ) {
+        if ( isset( $_GET['n1'] ) && is_numeric( $_GET['n1'] ) ) {
             $n1 = $_GET['n1'];
-            $op = $_GET['op'];
-            $n2 = $_GET['n2'];
-            $res = calculator( $n1, $op, $n2 );
+        } else {
+            $n1 = 0;
         }
-        ?>
+
+        $operations = ['+', '-', '*', '/', '^'];
+        if ( isset( $_GET['op'] ) && in_array( $_GET['op'], $operations) ) {
+            $op = $_GET['op'];
+        } else {
+            $op = $operations[0];
+        }
+
+        if ( isset( $_GET['n2'] ) && is_numeric( $_GET['n2'] ) ) {
+            $n2 = $_GET['n2'];
+        } else {
+            $n2 = 0;
+        }
+
+        $res = calculator( $n1, $op, $n2 );
+
+         ?>
 
         <form action="/homework3/calculator.php" method="get">
             <input type="text" name="n1" size="5" value="<?php echo $n1; ?>">
